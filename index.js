67d5ecd1968685bc2985d6b6a46b68677275dac6
Konstantin Shchelkanov <konstantin@quinncia.io>
2018-06-25 14:36:21 +0300

    delete bin folder

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-require('./bin/index.js');
+const Koa = require('koa');
+const Container = require('./src/container');
+
+// Create Koa instance
+const server = new Koa();
+
+// Register Service Container
+const app = new Container();
+
+server.context.container = app;
+server.context.make = app.make;
+
+module.exports = server;
